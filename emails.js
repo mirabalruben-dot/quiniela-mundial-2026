342bcb7d7e2cf5239b5606652d8c5733d4cb10d7
Ruben Mirabal <rubencho@Rubens-MacBook-Pro.local>
2026-06-09 01:55:38 -0400

    Emails desde quiniela@insuranceusa.us dominio verificado

--- emails.js
+++ emails.js
@@ -2,7 +2,7 @@ const { Resend } = require('resend');
 const db = require('./db');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const FROM_EMAIL = 'onboarding@resend.dev';
+const FROM_EMAIL = 'quiniela@insuranceusa.us';
 const FROM_NAME = 'Quiniela Insurance USA 2026';
 
 function getAllUserEmails() {
