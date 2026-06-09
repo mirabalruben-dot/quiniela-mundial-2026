cf33d4026dcbaa1dc66676d49c3ea5d35046a492
Ruben Mirabal <rubencho@Rubens-MacBook-Pro.local>
2026-06-09 01:49:05 -0400

    Fix email remitente a onboarding@resend.dev para plan gratuito

--- emails.js
+++ emails.js
@@ -2,7 +2,7 @@ const { Resend } = require('resend');
 const db = require('./db');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const FROM_EMAIL = 'quiniela@insuranceusa.us'; // cambiar si tienes dominio verificado en Resend
+const FROM_EMAIL = 'onboarding@resend.dev';
 const FROM_NAME = 'Quiniela Insurance USA 2026';
 
 function getAllUserEmails() {
